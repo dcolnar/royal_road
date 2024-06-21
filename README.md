eade2eeb8963be42f5e6ef9eee1069a0e3145b56
Derek Colnar <derek.colnar@gmail.com>
2024-06-21 16:43:48 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
-# royal_road
-Download Fictions from Royal Road for offline reading
+# RoyalRoad Chapter Scraper
+
+This is a web scraping script that scrapes chapters from a specified web novel on 
+Royal Road and saves them as HTML files. It includes functionality for logging, 
+and subsequent running from the last saved chapter since most on-going content
+releases on a schedule.
+
+## Dependencies
+
+The script requires the following Python libraries:
+
+- `os`
+- `re`
+- `time`
+- `typing`
+- `logging`
+- `sys`
+- `datetime`
+- `requests`
+- `bs4` (BeautifulSoup)
+- `urllib.parse`
+
+These dependencies can be installed using `pip`:
+
+```sh
+pip install requests beautifulsoup4
+```
+
+## Script Description
+The script performs the following steps:
+
+1. **Logging Configuration:** Sets up logging to log 
+messages to both a file (logs/logs.log) and the console.
+2. **Directory Creation:** Ensures necessary directories (logs and chapters) 
+exist.
+3. **Web Scraping Functions:** Fetches and processes HTML content from 
+the given URLs.
+4. **Chapter Processing:** Extracts chapter content and title, 
+and saves them as HTML files.
+5. **Navigation Handling:** Determines the URL of the next chapter 
+to continue scraping.
+6. **Resuming from Last Chapter:** Reads the last saved chapter URL to 
+resume scraping from where it left off.
+
+## Usage
+### Running the Script
+To run the script, execute the following command:
+
+```sh
+python scraper.py
+```
+### Initial Chapter Setup
+By default, the script attempts to resume from the last saved chapter. 
+If no saved chapter is found, it trys to use provided string, if you don't
+provide a string it will error out.
+You can specify a default initial chapter URL to start from:
+
+1. Create a file named `recent_chapter.txt` in the `logs` directory.
+2. Write the initial chapter URL into `recent_chapter.txt`.
+
+Alternatively, in the main function call give it a url string:
+```
+main(max_chapters=1000, delay=3, first_chapter=True, initial_url='url_goes_here')
+```
+## Configuration
+### Logging Configuration
+The logging configuration is defined in the `logging_config` dictionary. 
+Modify this dictionary to change logging behavior (e.g., log file path, 
+logging levels, formatting).
+
+### Main Function Parameters
+1. **max_chapters** (default: 1000): The maximum number of chapters to scrape.
+2. **first_chapter** (default: True): If you're on the first chapter for the fiction.
+Other runs or runs not starting from the first chapter set this to false.
+3. **delay** (default: 3 seconds): Delay between requests to avoid
+overloading the server.
+4. **initial_url** (default: None): This is the provided url for the start of the fiction.
+if you do not provide it, make sure to have the value set in the `recent_chapter.txt` file.
+
+These parameters can be changed by modifying the main function call 
+in the **if __name__ == '__main__'** block.
+
+### Example: Running with Initial Chapter from File
+1. Ensure the `logs/recent_chapter.txt` file contains the URL of the 
+initial chapter.
+2. Run the script as normal:
+```sh
+python scraper.py
+```
+
+
+## Notes
+- The script assumes the web novel follows a standard structure for chapter 
+navigation and content extraction.
+- Adjust the selectors in `get_chapter`, 
+`get_chapter_title`, and `get_next_chapter_link` functions 
+if the structure of the website changes.
+- The script includes a delay between requests to avoid 
+being blocked by the website. Adjust the delay parameter as needed.
+- Added url validation and have not tested it..... :shrug:
+
+## TODO
+- Get Title from page and use it to 
+store in specific directory for that fiction.
+Maybe create object for fiction title, chapter title, and 
+link in the beginning to reduce calls/keep state?
+- Fix issue with getting Next button using the length
+- Add create/check for files and log directories 
+if those are missing this breaks. The log one breaks when setting
+the logging_config which is super annoying.
+- Replace html save with pdf save, will be cleaner on offline reading
+- Add function or another script to merge files into one (maybe based on a book from author notes?)
+- Add font styling. My eyes are bleeding on phone... Style extension on brave ftw.
---
+++ scrape.py
@@ -0,0 +1,250 @@
+import os
+import re
+import time
+from typing import Optional
+import logging
+import logging.config
+import sys
+
+from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+# Define the logging configuration dictionary
+logging_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s - %(name)s(%(lineno)d) - %(levelname)s - %(message)s',
+            'datefmt': '%d/%m/%Y %I:%M:%S %p',
+        },
+    },
+    'handlers': {
+        'file': {
+            'class': 'logging.FileHandler',
+            'level': 'DEBUG',
+            'formatter': 'standard',
+            'filename': 'logs/logs.log',
+            'encoding': 'utf-8',
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'INFO',
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file', 'console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
+# Configure logging using the dictionary
+logging.config.dictConfig(logging_config)
+
+
+def create_directory(directory: str) -> None:
+    try:
+        os.makedirs(directory, exist_ok=True)
+        logging.debug(f"Directory '{directory}' created successfully or already exists.")
+    except OSError as e:
+        logging.error(f"Failed to create directory '{directory}': {e}")
+
+def get_page(url: str) -> BeautifulSoup:
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    return soup
+
+
+def remove_classes(soup: BeautifulSoup) -> BeautifulSoup:
+    for tag in soup.find_all(attrs={'class': True}):
+        del tag['class']
+    return soup
+
+
+def is_valid_url(url: str) -> bool:
+    parsed = urlparse(url)
+    return all([parsed.scheme, parsed.netloc])
+
+
+def sanitize_filename(title: str) -> str:
+    """
+    Sanitizes the title to create a valid filename by removing
+    punctuation and replacing spaces with underscores.
+
+    Args:
+        title (str): The title to sanitize.
+
+    Returns:
+        str: The sanitized filename.
+    """
+    title = re.sub(r'[^\w\s]', '', title)  # Remove all non-word characters (punctuation)
+    title = title.replace(' ', '_')        # Replace spaces with underscores
+    return title
+
+def get_chapter_content(soup: BeautifulSoup) -> BeautifulSoup:
+    initial_chapter_content = soup.find('div', class_='chapter-content')
+    chapter_content = remove_classes(initial_chapter_content)
+    return chapter_content
+
+
+def get_chapter_title(soup: BeautifulSoup) -> str:
+    title_block = soup.find('div', class_='fic-header')
+    title = title_block.find('h1').get_text()
+    return title
+
+
+# TODO: Use title to create specific folders for each fiction
+def get_fiction_title(soup: BeautifulSoup) -> str:
+    title_block = soup.find('div', class_='fic-header')
+    title = title_block.find('h2').get_text()
+    return title
+
+# TODO:
+'''
+Need to swap out saving the cleaned html for a pdf. Maybe even make a separate function
+to combine multiple chapters into a book. The point is for offline reading and removing all the ad junk'''
+def save_chapter(title: str, chapter: BeautifulSoup) -> None:
+    base_file_path: str = 'files'
+    create_directory(base_file_path)
+
+    # Extracting chapter number from the title
+    match = re.search(r'Chapter (\d+):', title)
+    chapter_number = match.group(1) if match else '1'
+
+    # Sanitize the title to create a valid filename
+    sanitized_title = sanitize_filename(title)
+
+    # Constructing file name using the extracted chapter number and sanitized title
+    file_name = f'{base_file_path}/{sanitized_title}.html'
+
+    html_content = f"<html><head><meta charset='utf-8'></head><body><h1>{title}</h1><hr />{chapter}</body></html>"
+    with open(file_name, 'w', encoding='utf-8') as file:
+        file.write(html_content)
+        logging.info(f'{title}: Chapter content has been written to file {file_name}.')
+
+
+# TODO:
+'''
+Currently the text for next button had white space around it, find_all doesn't like that
+Need to be able to search the text and determine if the button is disabled. Couldn't get regex 
+working correctly with: soup.find_all('a', string=re.compile('Next')) for some reason.
+Using this if len() stuff is weird, need to replace it with something better.
+'''
+
+
+def get_next_chapter_link(soup: BeautifulSoup, base_url: str, first_chapter: bool = True) -> Optional[str]:
+    nav_button = soup.find('div', class_='nav-buttons')
+    link_list = nav_button.find_all('a')
+    logging.debug(f'{base_url}: Links Found - {len(link_list)}')
+    logging.debug(f'{base_url}: Link Elements - {link_list}')
+
+    # If this is first run, there should be only next chapter button
+    if first_chapter == True:
+        link = base_url + link_list[0].get('href')
+        logging.debug(f'Next chapter: {link}')
+        return link
+    # As long as there are two links you are not at end of chapters
+    if len(link_list) == 2:
+        link = base_url + link_list[1].get('href')
+        logging.debug(f'Next chapter: {link}')
+        return link
+    # When there is one again, you made it to the end
+    logging.debug('There are no more links')
+    return None
+
+
+def save_recent_chapter(url: str):
+    create_directory('logs')
+    file_name = 'logs/recent_chapter.txt'
+    with open(file_name, 'w', encoding='utf-8') as file:
+        file.write(url)
+    logging.debug(f'Saved recent chapter at {datetime.now()} to {file_name}')
+
+
+def get_recent_chapter(default_chapter: str = None) -> str:
+    """
+    Retrieves the most recent chapter URL from a file or uses the default chapter URL if provided.
+
+    Args:
+        default_chapter (str): The default chapter URL to use if no recent chapter is found. Default is None.
+
+    Returns:
+        str: The URL of the recent chapter or the default chapter if valid.
+    """
+    file_name = 'logs/recent_chapter.txt'
+
+    try:
+        with open(file_name, 'r', encoding='utf-8') as file:
+            starting_chapter = file.read().strip()
+            if starting_chapter and is_valid_url(starting_chapter):
+                logging.debug("Retrieved recent chapter: %s", starting_chapter)
+                return starting_chapter
+            else:
+                logging.warning("Invalid URL found in recent chapter file or file is empty.")
+                logging.warning("Provided URL: %s", starting_chapter)
+    except FileNotFoundError:
+        logging.warning("Recent chapter file not found.")
+
+    if default_chapter is not None and is_valid_url(default_chapter):
+        logging.debug("Using default chapter URL: %s", default_chapter)
+        return default_chapter
+
+    logging.error("No valid recent chapter found and no valid default chapter provided. Exiting.")
+    sys.exit(1)
+
+
+def main(max_chapters: int = 1000, delay: int = 3, first_chapter: bool = True, initial_url: str = None) -> None:
+    """
+    Scrapes chapters from a web novel on RoyalRoad.
+
+    This function retrieves and saves chapters from a web novel hosted on RoyalRoad, starting
+    from the most recent chapter or the first chapter, depending on the `first_chapter` parameter.
+    It continues scraping until it reaches the specified `max_chapters` or there are no more chapters to retrieve.
+
+    Args:
+        max_chapters (int): Maximum number of chapters to scrape. Default is 1000.
+        first_chapter (bool): Whether to start from the first chapter. Default is True.
+        delay (int): Delay in seconds between requests to avoid spamming the website. Default is 3.
+        initial_url (str): The URL of the current chapter to start scraping from. Default is None.
+
+    Returns:
+        None
+    """
+    # TODO: This should probably be pulled from the url but im lazy.
+    base_url = 'https://www.royalroad.com'
+    current_url = get_recent_chapter(initial_url)
+    # Delay in seconds between requests
+    delay_between_requests = delay
+    # Number of pages to try initially
+    pages_to_try = max_chapters
+    pages_tried = 0
+
+    while current_url and pages_tried < pages_to_try:
+        soup = get_page(current_url)
+        chapter = get_chapter_content(soup)
+        chapter_title = get_chapter_title(soup)
+        save_chapter(chapter_title, chapter)
+        next_chapter_url = get_next_chapter_link(soup, base_url, first_chapter)
+        if next_chapter_url is None:
+            save_recent_chapter(current_url)
+        current_url = next_chapter_url
+        first_chapter = False
+        pages_tried += 1
+        # Delay to avoid spamming the website
+        time.sleep(delay_between_requests)
+
+    logging.info(f'Chapters scraped: {pages_tried}')
+
+
+if __name__ == '__main__':
+    # I like clear delimiters in running logs
+    logging.info('============ Starting Scrape============')
+    # Set initial_url string on first run and on subsequent runs remove it and set first_chapter to False
+    main(max_chapters=1000, delay=3, first_chapter=False, initial_url=None)
+    logging.info('============ Stopping Scrape============')
